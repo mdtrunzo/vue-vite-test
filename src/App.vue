84ccfac6e7fd3a976423c24419e026b4520cd0b3
Matías Trunzo <matiasdanieltrunzo@gmail.com>
2023-09-07 18:24:07 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template class="bg">
+  <div>
+    <a href="https://savaslabs.com" target="_blank">
+      <img
+        src="https://savaslabs.com/themes/custom/savas_2023/logo.svg"
+        class="logo"
+        alt="Savas Labs Logo"
+      />
+    </a>
+  </div>
+  <HelloWorld msg="Hello World!" />
+</template>
+
+<style scoped lang="scss">
+.logo {
+  height: 6em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+
+const hi = (msg: string) => {
+  alert(msg)
+}
+</script>
+
+<template>
+  <h1 class="hello">{{ msg }}</h1>
+  <div class="btn-container">
+    <button class="btn" @click="hi('Hello User!')">CLICK HERE!</button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '../../scss/style.scss';
+.read-the-docs {
+  color: #888;
+}
+</style>
